fd0fbc2802e7160467dfc350586f5ef3b0ac5863
Sabit56 <sabit.a.sirat@gmail.com>
2020-09-19 20:07:53 +0600

    Initial Commit

---
+++ vite_counter/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>The first ever image container is working. Rests are caching the first image building resources. So no changes are happening there!!</p>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
